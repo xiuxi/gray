6e27d5ef34e2826a19f17ff4567f7cb868b7712a
Ravi Kiran Boggavarapu <vsravikiran.b@gmail.com>
2016-04-04 01:39:19 +0530

    Big Changes 1
    The only thing that changed is, everything. Well, of course, that’s a
    caption by Apple. Okay, here are the list of changes:
    1) Better contours.
    2) Rectangle contours.
    3) The color changing line idea(beta).

--- main.cpp
+++ main.cpp
@@ -19,37 +19,61 @@
 using namespace cv;
 using namespace std;
 Ptr<BackgroundSubtractorMOG2> bg;
-int Keyboard;
+int Keyboard, TotalBlobs=0;
+Point pt1 = Point(0,170), pt2 = Point(320,170);
+//Some serious stuff down here.
 int main()
 {
-    bg = createBackgroundSubtractorMOG2();
-    Mat frame, roi;
-    Mat back;
-    Mat fore;
-    VideoCapture capture("doo.mp4");
-    bg->setNMixtures(3);
-    bg->setDetectShadows(true);
-    bg->setShadowValue(0);
-    bg->setShadowThreshold(0.5);
-    vector<vector<Point> > contours;
-    //namedWindow("Frame", WINDOW_AUTOSIZE);
-    //namedWindow("Foreground", WINDOW_AUTOSIZE);
-    while ((char)Keyboard!='q' && (char)Keyboard!=27)
+  bg = createBackgroundSubtractorMOG2();
+  Mat frame, roi;
+  Mat back;
+  Mat fore;
+  VideoCapture capture("dooagain.mp4");
+  bg->setNMixtures(3);
+  bg->setDetectShadows(true);
+  bg->setShadowValue(0);
+  bg->setShadowThreshold(0.5);
+  vector<vector<Point> > contours;
+  namedWindow("Frame Drawing",WINDOW_AUTOSIZE);
+  namedWindow("Frame", WINDOW_AUTOSIZE);
+  Point temp;
+  while ((char)Keyboard!='q' && (char)Keyboard!=27)
+  {
+    capture >> frame;
+    CBlobResult blobs;
+    roi = frame(Rect(76,199,113,40) );
+    bg->apply(frame, fore);
+    bg->getBackgroundImage(back);
+    erode(fore,fore,Mat());
+    dilate(fore,fore,Mat());
+    findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
+    vector<vector<Point> > contours_poly( contours.size() );
+    vector<Rect> boundRect( contours.size() );
+    vector<Point2f>center( contours.size() );
+    vector<float>radius( contours.size() );
+    int i=0;
+    for( ; i < contours.size(); i++ )
     {
-        capture >> frame;
-        roi = frame(Rect(46,25,144,84) );
-        bg->apply(roi, fore);
-        bg->getBackgroundImage(back);
-
-        erode(fore,fore,Mat());
-        dilate(fore,fore,Mat());
-        //findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-        //drawContours(roi,contours,-1,Scalar(0,0,255),2);
-        //roi = frame(Rect(46,25,144,84) );
-        //imshow("Frame",roi);
-        imshow("Foreground",fore);
-        //imshow("Background",back);
-        Keyboard=waitKey(1);
+      approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true ); //replace area with i.
+      boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+      minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
     }
-    return 0;
+    Mat drawing = Mat::zeros( frame.size(), CV_8UC3 );
+    for(i = 0; i< contours.size(); i++ )
+    {
+      rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), Scalar(0,0,255), 2, 8, 0 );
+      temp = boundRect[i].br();
+    }
+    cout<<temp.y<<endl;
+    if(temp.y<175 && temp.y>165){
+    line(frame, pt1, pt2, Scalar(0,255,0), 3, 8, 0);
+    }
+    else{
+    line(frame, pt1, pt2, Scalar(0,0,255), 3, 8, 0);
+    }
+    imshow("Frame Drawing",drawing);
+    imshow("Frame",frame);
+    Keyboard=waitKey(5);
+  }
+  return 0;
 }
